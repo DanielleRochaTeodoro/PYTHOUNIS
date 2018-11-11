7b586457a36260b9419440974864482bcb389d25
DanielleRochaTeodoro <39334693+DanielleRochaTeodoro@users.noreply.github.com>
2018-11-11 15:51:18 -0200

    Create ATIVIDADE02EX04.py

---
+++ ATIVIDADE02EX04.py
@@ -0,0 +1,9 @@
+contador = 1
+while contador<100:
+    if contador%2==0:
+        print("{} é par".format(contador))
+    else:
+        print("{} é impar".format(contador))
+
+
+    contador=contador+1
